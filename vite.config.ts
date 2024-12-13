9f53431f1efd04c85103006ae1e335eb22c1b0a5
Andrey Strelnikov <xoz9ih@outlook.com>
2024-12-13 14:19:40 +0300

    add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tasks/',
   plugins: [react()],
   server: {
     port: 3000, // Укажите нужный порт, например, 3000
